08039309c9082b5909408b0242321a46e07fc7a5
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2023-11-10 11:13:57 +0100

    Add check

--- js/task-2.js
+++ js/task-2.js
@@ -32,7 +32,6 @@
 
   const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
 
-
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
 console.log(getUsersWithFriend(allUsers, "Briana Decker"))
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
 console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
